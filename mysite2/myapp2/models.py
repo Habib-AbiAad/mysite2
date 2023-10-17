1ac4d14e5fb8e8b0bd5a745d5d8836e8d40df1d8
Habib-AbiAad <125749322+Habib-AbiAad@users.noreply.github.com>
2023-10-17 08:56:49 +0300

    Add files via upload

---
+++ mysite2/myapp2/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Contact(models.Model):
+    id = models.CharField(max_length=20, primary_key = True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    profession = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=20)
+    email = models.EmailField()
+    pass1 = models.CharField(max_length=100)
+    pass2 = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
